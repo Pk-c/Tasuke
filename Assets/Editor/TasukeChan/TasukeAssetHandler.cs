8e1bbef3107f312bc714ce1533d7e4625767a358
pkc <kaosumog@gmail.com>
2023-12-03 10:34:39 +0800

    [TASUKE] Initial commit

---
+++ Assets/Editor/TasukeChan/TasukeAssetHandler.cs
@@ -0,0 +1,23 @@
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+namespace TasukeChan
+{
+    public class TasukeAssetHandler
+    {
+        [OnOpenAsset]
+        public static bool OpenAsset(int instanceID, int line)
+        {
+            string path = AssetDatabase.GetAssetPath(instanceID);
+            UnityEngine.Object obj = EditorUtility.InstanceIDToObject(instanceID);
+
+            if (obj is TkData)
+            {
+                Board.Load(path,true);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Assets/Editor/TasukeChan/TasukeImporter.cs
@@ -0,0 +1,18 @@
+
+using UnityEditor;
+using UnityEngine;
+
+namespace TasukeChan
+{
+
+    [UnityEditor.AssetImporters.ScriptedImporter(1, "tk")]
+    public class TasukeImporter : UnityEditor.AssetImporters.ScriptedImporter
+    {
+        public override void OnImportAsset(UnityEditor.AssetImporters.AssetImportContext ctx)
+        {
+            var nodeWrapper = ScriptableObject.CreateInstance<TkData>();
+            ctx.AddObjectToAsset("main", nodeWrapper);
+            ctx.SetMainObject(nodeWrapper);
+        }
+    }
+}
